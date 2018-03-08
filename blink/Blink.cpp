b6cc5bd0206d907121880d9274ef7e8ec58b9e3e
Kandelion <rlagusdn6575@gmail.com>
2018-03-08 20:27:09 +0900

    Revert "11"
    This reverts commit d0662196fd017023b8f2f83bb2da832d2d096457.

--- blink/Blink.cpp
+++ blink/Blink.cpp
@@ -1,6 +1,6 @@
 #include "Blink.h"
 
-/* 내장된 LED의 Pull up/down 상태로 인해 HIGH 대신 LOW에서 점등, LOW 대신 HIGH에서 소등됨 *
+/* 내장된 LED의 Pull up/down 상태로 인해 HIGH 대신 LOW에서 점등, LOW 대신 HIGH에서 소등됨 * 
  * 이는 state의 변경에 따라 얼마든지 바뀔 수 있을 것으로 보이나 현재는 방법을 모르기 때문 *
  * 차후에 이러한 state의 변경을 이해하면 수정이 가능할 것으로 생각됨                      */
 
